4d01feedc767e9e9a05e38d149b98d1e70532ceb
barnerd <dibarner@gmail.com>
2025-02-18 21:27:56 -0800

    Fear and Hungry
    Adjust fear and hunger to have bonuses on laps and bonus on max_fear

--- game_board/game_board.gd
+++ game_board/game_board.gd
@@ -6,9 +6,12 @@ signal board_fear_changed(num: int)
 
 @export var board_spaces: Array[Node3D]
 
-@export var max_fear_threshold: int
+@export var max_fear: int
 @export var starting_fear: int
+@export var starting_max_fear_bonus: int
+
 var current_fear: int
+var current_max_fear_bonus: int
 
 
 func _init() -> void:
@@ -18,11 +21,15 @@ func _init() -> void:
 
 func _ready() -> void:
 	SignalBus.connect_to_signal("game_reset", game_reset)
+	SignalBus.connect_to_signal("lap_completed", on_lap_completed)
+	SignalBus.connect_to_signal("turn_taken", on_turn_taken)
 	SignalBus.connect_to_signal("story_variable_changed", on_story_variable_changed)
+	SignalBus.connect_to_signal("pet_hunger_starving", on_pet_starved)
 
 
 func game_reset() -> void:
 	current_fear = starting_fear
+	current_max_fear_bonus = starting_max_fear_bonus
 	board_fear_changed.emit(current_fear)
 
 
@@ -35,12 +42,30 @@ func on_story_variable_changed(variable_name: String, delta: float) -> void:
 		change_fear(delta)
 
 
+func on_lap_completed(_lap: int) -> void:
+	current_fear = 0
+	board_fear_changed.emit(current_fear)
+
+
+func on_turn_taken(_turns: int) -> void:
+	change_fear(1)
+
+
 func change_fear(_delta: int) -> void:
 	var prev_fear = current_fear
 	
 	current_fear += _delta
 	board_fear_changed.emit(current_fear)
 	
-	if (prev_fear < max_fear_threshold and 
-	current_fear > max_fear_threshold):
+	if current_fear >= (max_fear + current_max_fear_bonus):
+		current_fear = (max_fear + current_max_fear_bonus)
 		board_fear_max.emit()
+		destroy_tile(randi_range(0, board_spaces.size() - 1))
+
+
+func on_pet_starved() -> void:
+	destroy_tile(randi_range(0, board_spaces.size() - 1))
+
+
+func destroy_tile(_space: int) -> void:
+	print("Destroy tile #%d" % _space)
