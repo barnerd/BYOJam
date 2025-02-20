fe7b6e2f01a6b4c80436325635af09373288e10f
barnerd <dibarner@gmail.com>
2025-02-20 12:55:36 -0800

    Cleanup
    Moved some hard coded values to @export variables. Updated board_fear_changed signal to include current max fear as well.

--- game_board/game_board.gd
+++ game_board/game_board.gd
@@ -2,13 +2,7 @@ class_name GameBoard
 extends Node3D
 
 signal board_fear_max
-signal board_fear_changed(num: int)
-
-@export var board_spaces: Array[TileSpace]
-
-@export var max_fear: int
-@export var starting_fear: int
-@export var starting_max_fear_bonus: int
+signal board_fear_changed(num: int, max: int)
 
 const SPACE_TYPE_MATERIALS: Dictionary = {
 	TileSpace.TileType.FOOD: preload("res://ArtAssets/Materials/gameboard/food_green_mat.tres") as StandardMaterial3D,
@@ -16,6 +10,14 @@ const SPACE_TYPE_MATERIALS: Dictionary = {
 	TileSpace.TileType.UPGRADE: preload("res://ArtAssets/Materials/gameboard/upgrade_blue_mat.tres") as StandardMaterial3D,
 	TileSpace.TileType.DESTROYED: preload("res://ArtAssets/Materials/gameboard/destroyed_red_mat.tres") as StandardMaterial3D,
 }
+
+@export var board_spaces: Array[TileSpace]
+
+@export var max_fear: int
+@export var starting_fear: int
+@export var starting_max_fear_bonus: int
+
+@export var fear_per_turn: int
 
 var current_fear: int
 var current_max_fear_bonus: int
@@ -37,7 +39,7 @@ func _ready() -> void:
 func game_reset() -> void:
 	current_fear = starting_fear
 	current_max_fear_bonus = starting_max_fear_bonus
-	board_fear_changed.emit(current_fear)
+	board_fear_changed.emit(current_fear, max_fear + current_max_fear_bonus)
 	
 	for space in board_spaces:
 		space.set_type_material(SPACE_TYPE_MATERIALS[space.current_type])
@@ -54,16 +56,16 @@ func on_story_variable_changed(variable_name: String, delta: float) -> void:
 
 func on_lap_completed(_lap: int) -> void:
 	current_fear = 0
-	board_fear_changed.emit(current_fear)
+	board_fear_changed.emit(current_fear, max_fear + current_max_fear_bonus)
 
 
 func on_turn_taken(_turns: int) -> void:
-	change_fear(1)
+	change_fear(fear_per_turn)
 
 
 func change_fear(_delta: int) -> void:
 	current_fear += _delta
-	board_fear_changed.emit(current_fear)
+	board_fear_changed.emit(current_fear, max_fear + current_max_fear_bonus)
 	
 	if current_fear >= (max_fear + current_max_fear_bonus):
 		current_fear = (max_fear + current_max_fear_bonus)
@@ -80,10 +82,10 @@ func destroy_tile(_space: int) -> void:
 
 
 func perform_passing_effect(_space: int, _player: Player) -> void:
-	if _space > 0 and _space < board_spaces.size():
+	if _space >= 0 and _space < board_spaces.size():
 		board_spaces[_space].perform_passing_effect(_player)
 
 
 func perform_landing_effect(_space: int, _player: Player) -> void:
-	if _space > 0 and _space < board_spaces.size():
+	if _space >= 0 and _space < board_spaces.size():
 		board_spaces[_space].perform_landing_effect(_player)
