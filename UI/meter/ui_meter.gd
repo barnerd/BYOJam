5ea039fa825612a32105a5ca7013a13429153d67
barnerd <dibarner@gmail.com>
2025-03-01 17:59:34 -0800

    UI Meters
    Meters are more frenzy as they get closer to the red

--- UI/meter/ui_meter.gd
+++ UI/meter/ui_meter.gd
@@ -2,6 +2,7 @@
 extends Node2D
 
 @export var signal_name: String
+@export var is_right_high: bool = true
 
 var time_elapsed: float = 0
 var current_rotation: int = 1
@@ -20,15 +21,15 @@ func _ready() -> void:
 func set_value(_value: float) -> void:
 	current_value = clampf(_value, 0.0, 1.0)
 
-
 func start_new_tween() -> void:
+	var current_impulse = current_value if is_right_high else (1 - current_value)
+	# flip direction
 	current_rotation *= -1
-	var tween = get_tree().create_tween()
-	# " - .2 * PI" because of slice of arrow. remove with actual image
-	var random_rotation = current_rotation
-	random_rotation *= randf_range(0.5, 1.0) * PI / 100# * 4 * current_value
+	var random_rotation = current_rotation * (current_impulse + randf_range(1.0, 2.0)) * PI / 100
 	random_rotation += current_value * PI - 0.5 * PI
-	var random_time = randf_range(0.25, 0.5)# + 0.25 * current_value
+	var random_time = -0.65 * current_impulse + randf_range(0.75, 1.0)
+	
+	var tween = get_tree().create_tween()
 	tween.tween_property(arrow, "rotation", random_rotation, random_time)
 	tween.set_trans(Tween.TRANS_ELASTIC).set_ease(Tween.EASE_OUT)
 	tween.tween_callback(start_new_tween)
