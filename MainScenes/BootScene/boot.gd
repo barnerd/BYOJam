6e1a12071ca4cd8b4543ce22983ad9e87f31c336
barnerd <dibarner@gmail.com>
2025-03-03 11:05:52 -0800

    Build Issues

--- MainScenes/BootScene/boot.gd
+++ MainScenes/BootScene/boot.gd
@@ -3,7 +3,7 @@ extends Node
 # order matters
 @export var bootsplash_paths: Array[String] = [
 	"res://ProjectTemplate/SceneManager/BootSplashes/company_logo.tscn",
-	"res://ProjectTemplate/SceneManager/BootSplashes/godot_logo.tscn",
+	#"res://ProjectTemplate/SceneManager/BootSplashes/godot_logo.tscn",
 	]
 
 
